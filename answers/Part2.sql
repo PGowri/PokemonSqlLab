433bcc59b8a8d15a94f626e0912a8b83f91eb7f2
Gowri <gowri@ZipcodersMBP64.local>
2023-12-02 12:58:38 -0500

    finished the part2

---
+++ answers/Part2.sql
@@ -0,0 +1,7 @@
+ SELECT * FROM types;
+ SELECT name FROM pokemons  WHERE id = 45;
+ SELECT count(*) FROM pokemons;
+ SELECT count(*) FROM types;
+ SELECT count(*) FROM pokemons WHERE secondary_type IS NOT null;
+
+
